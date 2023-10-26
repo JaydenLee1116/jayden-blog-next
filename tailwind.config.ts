d8eb1bddcafe9a52691db728bc49fe49c916c426
JaydenLee1116 <vv55adss@gmail.com>
2023-10-26 11:17:26 +0900

    🔧 chore #1: favicon 이미지 설치
    - jayden-0 색상 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,13 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        'jayden-0': '#febd57',
+      },
+    },
+  },
   plugins: [],
 };
 export default config;
