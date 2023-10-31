721f297c69d0841897915bb6b68587c4cd1e83a4
JaydenLee1116 <vv55adss@gmail.com>
2023-11-01 00:40:01 +0900

    🔧 chore #1: contentlayer 설치 및 설정

---
+++ contentlayer.config.ts
@@ -0,0 +1,22 @@
+import { defineDocumentType, makeSource } from 'contentlayer/source-files';
+
+export const Post = defineDocumentType(() => ({
+  name: 'Post',
+  filePathPattern: `**/*.md`,
+  fields: {
+    emoji: { type: 'string', required: true },
+    title: { type: 'string', required: true },
+    date: { type: 'string', required: true },
+    author: { type: 'string', required: true },
+    tags: { type: 'string', required: true },
+    categories: { type: 'string', required: true },
+    thumbnail: { type: 'string', required: false },
+  },
+  computedFields: {
+    url: { type: 'string', resolve: post => `/posts/${post._raw.flattenedPath}` },
+    tagList: { type: 'string', resolve: post => post.tags.split(' ') },
+    categoryList: { type: 'string', resolve: post => post.categories.split(' ') },
+  },
+}));
+
+export default makeSource({ contentDirPath: 'posts', documentTypes: [Post] });
