aa9f0c5e723b119497fbf03d91b4278220871b82
JaydenLee1116 <vv55adss@gmail.com>
2023-11-01 19:01:33 +0900

    ✨ feat #6: Markdown 컴포넌트 구현

---
+++ src/components/Markdown/index.tsx
@@ -0,0 +1,44 @@
+'use client';
+import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { materialDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import remarkGfm from 'remark-gfm';
+import Image from 'next/image';
+
+type Props = {
+  children: string;
+};
+
+export default function Markdown({ children }: Props) {
+  const markdownClassName = {
+    headings: 'prose-headings:text-slate-700 dark:prose-headings:text-slate-100',
+    code: 'prose-code:text-inflearn-0',
+    blockquote: 'prose-blockquote:text-jayden-0',
+    a: 'prose-a:text-black-0 dark:prose-a:text-white',
+  };
+
+  // TODO: prose-base, sm, lg, xl 등 사용하여 글 축소 및 확대 기능 고려해보기
+  return (
+    <ReactMarkdown
+      className={`prose-base w-full text-slate-700 dark:text-slate-100 ${markdownClassName.headings} ${markdownClassName.code} ${markdownClassName.blockquote} ${markdownClassName.a}`}
+      remarkPlugins={[remarkGfm]}
+      components={{
+        code({ node, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || '');
+          return match ? (
+            <SyntaxHighlighter language={match[1]} PreTag="div" style={materialDark}>
+              {String(children).replace(/\n$/, '')}
+            </SyntaxHighlighter>
+          ) : (
+            <code {...props}>{children}</code>
+          );
+        },
+        img: image => (
+          <Image src={image.src || ''} alt={image.alt || ''} width={500} height={300} />
+        ),
+      }}
+    >
+      {children}
+    </ReactMarkdown>
+  );
+}
