9ca8478de17a38fb52a9f8441afcefa64fda63dc
JaydenLee1116 <vv55adss@gmail.com>
2023-11-22 23:13:21 +0900

    ✨ feat #6: 서치바 반응형 추가
    - 레이아웃 조정

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -10,7 +10,7 @@ export default function SearchBar({ value, onChange }: Props) {
       value={value}
       onChange={onChange}
       placeholder="검색어를 입력해주세요 :)"
-      className="sticky top-9 z-10 h-20 w-full rounded-lg border-2 border-slate-800 bg-white pl-4 text-4xl dark:border-white dark:bg-slate-800"
+      className="sticky top-20 z-10 h-10 w-full rounded-lg border border-slate-800 bg-white pl-2 text-xl dark:border-white dark:bg-slate-800 sm:h-20 sm:border-2 sm:pl-4 sm:text-4xl"
     />
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -20,7 +20,7 @@ const pages = [
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-10 flex flex-col items-center justify-between bg-slate-100 bg-opacity-10 backdrop-blur-sm dark:bg-slate-700 dark:bg-opacity-10 sm:flex-row">
+    <header className="sticky top-0 z-10 flex h-20 flex-col items-center justify-between bg-slate-100 bg-opacity-10 backdrop-blur-sm dark:bg-slate-700 dark:bg-opacity-10 sm:flex-row">
       <Link href="/" className="flex items-center gap-2">
         <Image src="/images/favicon.png" width="36" height="36" alt="favicon" priority />
         <h1 className="truncate whitespace-nowrap text-2xl font-bold text-jayden-0 sm:text-3xl">
