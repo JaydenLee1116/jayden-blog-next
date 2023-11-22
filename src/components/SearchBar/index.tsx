e31ab255dcdd7a9c710e72c036d643f0ec82f5b2
JaydenLee1116 <vv55adss@gmail.com>
2023-11-23 01:06:08 +0900

    ✨ feat #6: 검색 결과 없음 추가
    - vue 튜토리얼 오타 수정
    - SearchBar 컴포넌트 파일 공백 제거

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -9,7 +9,7 @@ export default function SearchBar({ value, onChange }: Props) {
       type="text"
       value={value}
       onChange={onChange}
-      placeholder="검색어를 입력해주세요 :)"
+      placeholder="검색어를 입력해주세요!"
       className="sticky top-20 z-10 h-10 w-full rounded-lg border border-slate-800 bg-white pl-2 text-xl dark:border-white dark:bg-slate-800 sm:h-20 sm:border-2 sm:pl-4 sm:text-4xl"
     />
   );
