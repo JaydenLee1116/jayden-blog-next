1106ed5523191b107eb83b3e9cd7c379aa2805fb
JaydenLee1116 <vv55adss@gmail.com>
2023-11-23 19:24:41 +0900

    ✨ feat #6: Posts 페이지 ProgressBar 기능 추가
     useReadingProgress 커스텀 훅 구현

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 import ThemeSwitcher from '../ThemeSwitcher';
+import ProgressBar from '../ProgressBar';
 
 const pages = [
   {
@@ -20,23 +21,26 @@ const pages = [
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-10 flex h-20 flex-col items-center justify-between bg-slate-100 bg-opacity-10 backdrop-blur-sm dark:bg-slate-700 dark:bg-opacity-10 sm:flex-row">
-      <Link href="/" className="flex items-center gap-2">
-        <Image src="/images/favicon.png" width="36" height="36" alt="favicon" priority />
-        <h1 className="truncate whitespace-nowrap text-2xl font-bold text-jayden-0 sm:text-3xl">
-          {'Jayden { do: smite }'}
-        </h1>
-      </Link>
-      <div className="flex gap-8">
-        <nav className="flex justify-between gap-6">
-          {pages.map(page => (
-            <Link href={page.href} key={page.href}>
-              <span className="text-xl font-bold sm:text-2xl">{page.name}</span>
-            </Link>
-          ))}
-        </nav>
-        <ThemeSwitcher />
-      </div>
-    </header>
+    <div className="sticky top-0 z-10 flex flex-col ">
+      <header className="sticky top-0 z-10 flex h-20 flex-col items-center justify-between bg-slate-100 bg-opacity-10 backdrop-blur-sm dark:bg-slate-700 dark:bg-opacity-10 sm:flex-row">
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/images/favicon.png" width="36" height="36" alt="favicon" priority />
+          <h1 className="truncate whitespace-nowrap text-2xl font-bold text-jayden-0 sm:text-3xl">
+            {'Jayden { do: smite }'}
+          </h1>
+        </Link>
+        <div className="flex gap-8">
+          <nav className="flex justify-between gap-6">
+            {pages.map(page => (
+              <Link href={page.href} key={page.href}>
+                <span className="text-xl font-bold sm:text-2xl">{page.name}</span>
+              </Link>
+            ))}
+          </nav>
+          <ThemeSwitcher />
+        </div>
+      </header>
+      <ProgressBar />
+    </div>
   );
 }
---
+++ src/components/ProgressBar/index.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { useReadingProgress } from '@/hooks/useReadingProgress';
+import { usePathname } from 'next/navigation';
+
+export default function ProgressBar() {
+  const completion = useReadingProgress();
+  const pathname = usePathname();
+  const isHome = pathname === '/';
+
+  return (
+    <>
+      {isHome || (
+        <div className="sticky top-0 z-20">
+          <span
+            style={{
+              transform: `translateX(${completion - 104}%)`,
+            }}
+            className="absolute bottom-0 h-1 w-screen bg-jayden-0 transition-transform duration-75"
+          />
+        </div>
+      )}
+    </>
+  );
+}
