f93bb3547330ea70f7dbb04189e88dc37202c3c9
JaydenLee1116 <vv55adss@gmail.com>
2024-08-22 23:52:30 +0900

    design: header 호버 시 투명도 변화 추가

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -19,7 +19,7 @@ export default function Header() {
   return (
     <header className="sticky top-0 z-10 flex h-fit flex-col justify-around">
       <div className="flex h-20 flex-col items-center justify-center bg-slate-100 bg-opacity-10 backdrop-blur-sm dark:bg-slate-700 dark:bg-opacity-10 sm:flex-row sm:justify-between">
-        <Link href="/" className="flex items-center gap-2">
+        <Link href="/" className="flex items-center gap-2 hover:opacity-70">
           <Image src="/images/favicon.png" width="36" height="36" alt="favicon" priority />
           <h1 className="truncate whitespace-nowrap text-2xl font-bold text-jayden-0 sm:text-3xl">
             {'Jayden { do: smite }'}
@@ -29,7 +29,7 @@ export default function Header() {
           <nav className="flex justify-between gap-6">
             {pages.map(page => (
               <Link href={page.href} key={page.href}>
-                <span className="text-xl font-bold sm:text-2xl">{page.name}</span>
+                <span className="text-xl font-bold hover:opacity-70 sm:text-2xl">{page.name}</span>
               </Link>
             ))}
           </nav>
